0a3f0b24d16cdd00e14c1ecee94fbff22e46f220
ADEDANCE <uthmanadedance@gmail.com>
2026-01-28 11:46:09 +0100

    Feat(add add to cart and buy now button)

--- lib/screens/home/product_details.dart
+++ lib/screens/home/product_details.dart
@@ -7,6 +7,7 @@ import 'package:shoppe/screens/common_widgets/review_widget.dart';
 int selectedIndex = 1;
 int quantity = 0;
 int selecteddelivery = 0;
+bool isFavorite = false;
 
 class ProductDetails extends StatefulWidget {
   final Map product;
@@ -64,163 +65,208 @@ class _ProductDetailsState extends State<ProductDetails> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Expanded(
-          child: SingleChildScrollView(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                SizedBox(
-                  width: double.infinity, //  full screen width
-                  height: 500.h,
-                  child: Image.asset(
-                    widget.product['image'],
-                    fit: BoxFit.cover,
-                  ),
-                ),
-                Padding(
-                  padding: EdgeInsets.symmetric(
-                    vertical: 20.h,
-                    horizontal: 16.w,
-                  ),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        "\$${widget.product["price"]}",
-                        style: TextStyle(
-                          fontSize: 24,
-                          fontWeight: FontWeight.bold,
-                        ),
+        child: Column(
+          children: [
+            Expanded(
+              child: SingleChildScrollView(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    SizedBox(
+                      width: double.infinity, //  full screen width
+                      height: 500.h,
+                      child: Image.asset(
+                        widget.product['image'],
+                        fit: BoxFit.cover,
                       ),
-
-                      Text(
-                        widget.product['description'],
-                        style: TextStyle(
-                          fontSize: 18,
-                          fontWeight: FontWeight.normal,
-                        ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.symmetric(
+                        vertical: 20.h,
+                        horizontal: 16.w,
                       ),
-                      SizedBox(height: 10),
-
-                      Text(
-                        "Size",
-                        style: TextStyle(
-                          fontSize: 20,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          _selectableButton('S', 0),
-                          _selectableButton('M', 1),
-                          _selectableButton('L', 2),
-                          _selectableButton('XL', 3),
-                          //    _SelectableButton('XXL', 4),
-                        ],
-                      ),
-                      SizedBox(height: 10),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
                           Text(
-                            "Quantity",
+                            "\$${widget.product["price"]}",
+                            style: TextStyle(
+                              fontSize: 24,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+
+                          Text(
+                            widget.product['description'],
+                            style: TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.normal,
+                            ),
+                          ),
+                          SizedBox(height: 10),
+
+                          Text(
+                            "Size",
                             style: TextStyle(
                               fontSize: 20,
                               fontWeight: FontWeight.bold,
                             ),
                           ),
+
                           Row(
                             mainAxisAlignment: MainAxisAlignment.spaceBetween,
                             children: [
-                              _quantityButton('-', () {
-                                setState(() {
-                                  if (quantity > 1) {
-                                    quantity = quantity - 1;
-                                  }
-                                });
-                              }),
-                              SizedBox(width: 8.w),
-                              Container(
-                                width: 74.w,
-                                height: 40.h,
-                                decoration: BoxDecoration(
-                                  color: Color(0xFFE5EBFC),
-                                  borderRadius: BorderRadius.circular(10.r),
-                                  // shape: Border()
-                                ),
-                                child: Text(
-                                  textAlign: TextAlign.center,
-                                  quantity.toString(),
-                                  style: TextStyle(
-                                    fontSize: 25,
-                                    fontWeight: FontWeight.normal,
-                                  ),
-                                ),
-                              ),
-                              SizedBox(width: 8.w),
-                              _quantityButton('+', () {
-                                setState(() {
-                                  quantity = quantity + 1;
-                                });
-                              }),
+                              _selectableButton('S', 0),
+                              _selectableButton('M', 1),
+                              _selectableButton('L', 2),
+                              _selectableButton('XL', 3),
+                              //    _SelectableButton('XXL', 4),
                             ],
                           ),
+                          SizedBox(height: 10),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Text(
+                                "Quantity",
+                                style: TextStyle(
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: [
+                                  _quantityButton('-', () {
+                                    setState(() {
+                                      if (quantity > 1) {
+                                        quantity = quantity - 1;
+                                      }
+                                    });
+                                  }),
+                                  SizedBox(width: 8.w),
+                                  Container(
+                                    width: 74.w,
+                                    height: 40.h,
+                                    decoration: BoxDecoration(
+                                      color: Color(0xFFE5EBFC),
+                                      borderRadius: BorderRadius.circular(10.r),
+                                      // shape: Border()
+                                    ),
+                                    child: Text(
+                                      textAlign: TextAlign.center,
+                                      quantity.toString(),
+                                      style: TextStyle(
+                                        fontSize: 25,
+                                        fontWeight: FontWeight.normal,
+                                      ),
+                                    ),
+                                  ),
+                                  SizedBox(width: 8.w),
+                                  _quantityButton('+', () {
+                                    setState(() {
+                                      quantity = quantity + 1;
+                                    });
+                                  }),
+                                ],
+                              ),
+                            ],
+                          ),
+                          SizedBox(height: 8.h),
+                          Text(
+                            "Delivery",
+                            style: TextStyle(
+                              fontSize: 20.sp,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          DeliveryType(
+                            isdeliverySelected: selecteddelivery == 0,
+                            onTap: () {
+                              setState(() {
+                                selecteddelivery = 0;
+                              });
+                            },
+
+                            title: "Standard",
+                            duration: "5-7 days",
+                            price: "\$3,00",
+                          ),
+                          DeliveryType(
+                            isdeliverySelected: selecteddelivery == 1,
+                            onTap: () {
+                              setState(() {
+                                selecteddelivery = 1;
+                              });
+                            },
+
+                            title: "Standard",
+                            duration: "1-2 days",
+                            price: "\$12,00",
+                          ),
+                          SizedBox(height: 8.h),
+                          Text(
+                            "Rating & Reviews",
+                            style: TextStyle(
+                              fontSize: 20.sp,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          ReviewWidget(),
+                          SizedBox(height: 25.h),
+                          ButtonWidget(
+                            width: double.infinity,
+                            height: 45,
+                            text: "View All Reviews",
+                            onPressed: () {},
+                            color: Color(0XFF004CFF),
+                          ),
+                          //   SizedBox(height: 25.h),
                         ],
                       ),
-                      SizedBox(height: 8.h),
-                      Text(
-                        "Delivery",
-                        style: TextStyle(
-                          fontSize: 20.sp,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      DeliveryType(
-                        isdeliverySelected: selecteddelivery == 0,
-                        onTap: () {
-                          setState(() {
-                            selecteddelivery = 0;
-                          });
-                        },
-
-                        title: "Standard",
-                        duration: "5-7 days",
-                        price: "\$3,00",
-                      ),
-                      DeliveryType(
-                        isdeliverySelected: selecteddelivery == 1,
-                        onTap: () {
-                          setState(() {
-                            selecteddelivery = 1;
-                          });
-                        },
-
-                        title: "Standard",
-                        duration: "1-2 days",
-                        price: "\$12,00",
-                      ),
-                      SizedBox(height: 8.h),
-                      Text(
-                        "Rating & Reviews",
-                        style: TextStyle(
-                          fontSize: 20.sp,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      ReviewWidget(),
-                      ButtonWidget(
-                        text: "View All Reviews",
-                        onPressed: () {},
-                        color: Color(0XFF004CFF),
-                      ),
-                    ],
-                  ),
+                    ),
+                  ],
                 ),
-              ],
+              ),
             ),
-          ),
+
+            Padding(
+              padding: EdgeInsets.symmetric(vertical: 20.h, horizontal: 16.w),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  IconButton(
+                    icon: Icon(
+                      isFavorite ? Icons.favorite : Icons.favorite_border,
+                      size: 25,
+                      color: isFavorite ? Colors.blue : Colors.grey,
+                    ),
+                    onPressed: () {
+                      setState(() {
+                        isFavorite = !isFavorite;
+                      });
+                    },
+                  ),
+
+                  ButtonWidget(
+                    width: 128.w,
+                    height: 58.h,
+                    text: "Add to cart",
+                    onPressed: () {},
+                    color: Color(0XFF000000),
+                  ),
+                  ButtonWidget(
+                    width: 160.w,
+                    height: 60.h,
+                    text: "Buy now",
+                    onPressed: () {},
+                    color: Color(0XFF004CFF),
+                  ),
+                ],
+              ),
+            ),
+          ],
         ),
       ),
     );
