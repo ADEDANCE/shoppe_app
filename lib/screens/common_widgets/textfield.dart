78f81bd3b43dde5ff68c3e3537aaa62420b8e1e8
ADEDANCE <uthmanadedance@gmail.com>
2026-02-02 18:27:54 +0100

    import images

--- lib/screens/common_widgets/textfield.dart
+++ lib/screens/common_widgets/textfield.dart
@@ -1,17 +1,17 @@
 import 'package:flutter/material.dart';
 
-
 class Textfield extends StatefulWidget {
-    final TextEditingController controller;
+  final TextEditingController controller;
   final String hintText;
-  final Color ? fillColor;
- // final Widget?  suffixIcon;
- final bool issuffixIcon;
-  const Textfield({super.key,
-   required this.controller,
- required this.hintText,
- this.fillColor,
- this.issuffixIcon = false,
+  final Color? fillColor;
+  // final Widget?  suffixIcon;
+  final bool issuffixIcon;
+  const Textfield({
+    super.key,
+    required this.controller,
+    required this.hintText,
+    this.fillColor,
+    this.issuffixIcon = false,
   });
 
   @override
@@ -19,41 +19,39 @@ class Textfield extends StatefulWidget {
 }
 
 class _TextfieldState extends State<Textfield> {
-    bool _isObscure = true;
+  bool _isObscure = true;
   @override
   Widget build(BuildContext context) {
-   return TextFormField(
-             controller: widget.controller,   
-             obscureText:  widget.issuffixIcon ? _isObscure : false, 
-            decoration: InputDecoration(
-              hintText: widget.hintText,
-              
-              filled: true,
-              fillColor: widget.fillColor,
-              enabledBorder: OutlineInputBorder(
-                 borderRadius: BorderRadius.circular(50),
-                 borderSide: BorderSide.none
-              ),
-              focusedBorder:  OutlineInputBorder(
-                 borderRadius: BorderRadius.circular(50),
-                borderSide: BorderSide.none
-              ),
-              
-              suffixIcon: widget.issuffixIcon
+    return TextFormField(
+      controller: widget.controller,
+      obscureText: widget.issuffixIcon ? _isObscure : false,
+      decoration: InputDecoration(
+        hintText: widget.hintText,
+
+        filled: true,
+        fillColor: widget.fillColor,
+        enabledBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(50),
+          borderSide: BorderSide.none,
+        ),
+        focusedBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(50),
+          borderSide: BorderSide.none,
+        ),
+
+        suffixIcon: widget.issuffixIcon
             ? IconButton(
                 icon: Icon(
-                  _isObscure ?Icons.visibility_off : Icons.remove_red_eye ,
+                  _isObscure ? Icons.visibility_off : Icons.remove_red_eye,
                 ),
                 onPressed: () {
                   setState(() {
-                  _isObscure = !_isObscure;
+                    _isObscure = !_isObscure;
                   });
                 },
               )
             : null, // only show if isPassword is true
-      
-            ),
-            
-          );
+      ),
+    );
   }
 }
