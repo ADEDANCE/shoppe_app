44d5d5c378e77b9e72ff70d52a807cdb2dbf3d0d
Adedance <uthmanadedance@gmail.com>
2026-02-18 22:35:16 +0100

    clean up

--- lib/screens/user_side/services/product_categories.dart
+++ lib/screens/user_side/services/product_categories.dart
@@ -1,4 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class FirestoreService {
   final FirebaseFirestore _db = FirebaseFirestore.instance;
@@ -16,5 +17,10 @@ class FirestoreService {
         .collection('products')
         .where('category', isEqualTo: categoryName)
         .snapshots();
+  }
+
+  Stream<QuerySnapshot> getCart() {
+    final uid = FirebaseAuth.instance.currentUser!.uid;
+    return _db.collection("users").doc(uid).collection("cart").snapshots();
   }
 }
