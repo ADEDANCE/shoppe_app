bec3db8bb69205718ae1dad234d97ed0c3d0a8db
Adedance <uthmanadedance@gmail.com>
2026-07-04 18:55:13 +0100

    Feat:Create reusable color

---
+++ lib/theme/app_colors.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  // Private constructor
+  AppColors._();
+
+  // Brand Colors
+  static const Color primaryBlue = Color(0xFF2563EB);
+  static const Color secondaryBlue = Color(0xFF60A5FA);
+
+  // Background Colors
+  static const Color background = Color(0xFFF8FAFC);
+  static const Color secondaryWhite = Color(0xFFD8DEEC);
+
+  // Neutral Colors
+  static const Color white = Colors.white;
+  static const Color black = Colors.black;
+  static const Color grey = Colors.grey;
+
+  // Status Colors
+  static const Color success = Colors.green;
+  static const Color error = Colors.red;
+  static const Color warning = Colors.orange;
+}
