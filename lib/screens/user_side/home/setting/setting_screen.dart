2983670d9f996f5feb110f11881294ced2ca197c
Adedance <uthmanadedance@gmail.com>
2026-07-05 17:48:53 +0100

    Edit setting screen

--- lib/screens/user_side/home/setting/setting_screen.dart
+++ lib/screens/user_side/home/setting/setting_screen.dart
@@ -7,6 +7,7 @@ import 'package:shoppe/screens/user_side/home/setting/add_card_screen.dart';
 import 'package:shoppe/screens/user_side/home/setting/language_screen.dart';
 import 'package:shoppe/screens/user_side/home/setting/profile_setting_screen.dart';
 import 'package:shoppe/screens/user_side/home/setting/shippingaddress_screen.dart';
+import 'package:shoppe/theme/app_colors.dart';
 
 class SettingScreen extends StatefulWidget {
   const SettingScreen({super.key});
@@ -36,197 +37,205 @@ class _SettingScreenState extends State<SettingScreen> {
             child: SingleChildScrollView(
               child: Column(
                 children: [
-                  Align(
-                    alignment: Alignment.centerLeft,
-                    child: Text(
-                      "Personal",
-                      style: TextStyle(
-                        fontSize: 20.sp,
-                        fontWeight: FontWeight.bold,
-                      ),
+                  Container(
+                    padding: EdgeInsets.all(14),
+                    decoration: BoxDecoration(
+                      color: AppColors.secondaryWhite,
+                      borderRadius: BorderRadius.circular(16.r),
                     ),
-                  ),
-                  SizedBox(height: 30.h),
-                  DropdownRow(
-                    title: "Profile",
-                    onTap: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (_) => ProfileSettingScreen(),
+                    child: Column(
+                      children: [
+                        Align(
+                          alignment: Alignment.centerLeft,
+                          child: Text(
+                            "Personal",
+                            style: TextStyle(
+                              fontSize: 20.sp,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
                         ),
-                      );
-                    },
-                  ),
-                  SizedBox(height: 30.h),
-                  DropdownRow(
-                    title: "Shipping Address",
-                    onTap: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (_) => ShippingaddressScreen(),
-                        ),
-                      );
-                    },
-                  ),
-                  SizedBox(height: 30.h),
-                  DropdownRow(
-                    title: "Payment methods",
-                    onTap: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(builder: (_) => AddCardScreen()),
-                      );
-                    },
-                  ),
-                  SizedBox(height: 30.h),
-                  Align(
-                    alignment: Alignment.centerLeft,
-                    child: Text(
-                      "Shop",
-                      style: TextStyle(
-                        fontSize: 20.sp,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                  ),
-                  SizedBox(height: 30.h),
-                  DropdownRow(
-                    title: "Country",
-                    subtitle: 'Vietnam',
-                    onTap: () {},
-                  ),
-                  SizedBox(height: 30.h),
-                  DropdownRow(
-                    title: "Currency",
-                    subtitle: '\$USD',
-                    onTap: () {},
-                  ),
-                  SizedBox(height: 30.h),
-                  DropdownRow(title: "Sizes", subtitle: 'UK', onTap: () {}),
-                  SizedBox(height: 30.h),
-                  DropdownRow(title: "Terms and Conditions", onTap: () {}),
-                  SizedBox(height: 30.h),
-                  Align(
-                    alignment: Alignment.centerLeft,
-                    child: Text(
-                      "Personal",
-                      style: TextStyle(
-                        fontSize: 20.sp,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                  ),
-                  SizedBox(height: 30.h),
-                  DropdownRow(
-                    title: "Language",
-                    subtitle: 'English',
-                    onTap: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(builder: (_) => LanguageScreen()),
-                      );
-                    },
-                  ),
-                  SizedBox(height: 30.h),
-                  DropdownRow(
-                    title: "About Shoppe",
-                    onTap: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(builder: (_) => AboutUsScreen()),
-                      );
-                    },
-                  ),
-                  SizedBox(height: 50.h),
-                  Align(
-                    alignment: Alignment.centerLeft,
-                    child: GestureDetector(
-                      onTap: () {
-                        showDialog(
-                          context: context,
-                          builder: (context) {
-                            return Dialog(
-                              backgroundColor: Color(0xFFFFFFFF),
-                              shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(16),
-                              ),
 
-                              child: SizedBox(
-                                width: double.infinity,
-                                child: Padding(
-                                  padding: const EdgeInsets.all(20),
-                                  child: Column(
-                                    mainAxisSize: MainAxisSize.min,
-                                    children: [
-                                      Image.asset(
-                                        "assets/images/bluenav-btn.png",
-                                        width: 40.w,
-                                        height: 40.h,
-                                      ),
-                                      Text(
-                                        textAlign: TextAlign.center,
-                                        "You are going to delete your account",
-                                        style: TextStyle(
-                                          fontSize: 20.sp,
-                                          fontWeight: FontWeight.bold,
-                                        ),
-                                      ),
-                                      Text(
-                                        textAlign: TextAlign.center,
-                                        "You won't be able to restore your data",
-                                        style: TextStyle(
-                                          fontSize: 15.sp,
-                                          fontWeight: FontWeight.bold,
-                                        ),
-                                      ),
-                                      Row(
-                                        mainAxisAlignment:
-                                            MainAxisAlignment.spaceBetween,
-                                        children: [
-                                          SizedBox(
-                                            width: 140.w,
-                                            child: ButtonWidget(
-                                              text: "Cancel",
-                                              onPressed: () {},
-                                              textcolor: Colors.white,
-                                              color: Colors.black,
-                                              height: 40.h,
-                                              width: double.infinity,
-                                            ),
-                                          ),
-                                          SizedBox(
-                                            width: 140.w,
-                                            child: ButtonWidget(
-                                              text: "Delete",
-                                              onPressed: () {},
-                                              textcolor: Colors.white,
-                                              color: Colors.redAccent,
-                                              height: 40.h,
-                                              width: double.infinity,
-                                            ),
-                                          ),
-                                        ],
-                                      ),
-                                    ],
-                                  ),
-                                ),
+                        SizedBox(height: 30.h),
+
+                        DropdownRow(
+                          title: "Profile",
+                          onTap: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (_) => ProfileSettingScreen(),
                               ),
                             );
                           },
-                        );
-                      },
-                      child: Text(
-                        "Delete My Account",
-                        style: TextStyle(
-                          color: Colors.redAccent,
-                          fontSize: 20.sp,
-                          fontWeight: FontWeight.bold,
                         ),
-                      ),
+                        SizedBox(height: 30.h),
+                        DropdownRow(
+                          title: "Shipping Address",
+                          onTap: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (_) => ShippingaddressScreen(),
+                              ),
+                            );
+                          },
+                        ),
+                        SizedBox(height: 30.h),
+                        DropdownRow(
+                          title: "Payment methods",
+                          onTap: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (_) => AddCardScreen(),
+                              ),
+                            );
+                          },
+                        ),
+                      ],
                     ),
                   ),
+
+                  SizedBox(height: 50.h),
+                  Container(
+                    padding: EdgeInsets.all(14),
+                    decoration: BoxDecoration(
+                      color: AppColors.secondaryWhite,
+                      borderRadius: BorderRadius.circular(16.r),
+                    ),
+                    child: Column(
+                      children: [
+                        Align(
+                          alignment: Alignment.centerLeft,
+                          child: Text(
+                            "Support",
+                            style: TextStyle(
+                              fontSize: 20.sp,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+
+                        SizedBox(height: 30.h),
+
+                        DropdownRow(
+                          title: "Language",
+                          subtitle: 'English',
+                          onTap: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (_) => LanguageScreen(),
+                              ),
+                            );
+                          },
+                        ),
+
+                        SizedBox(height: 30.h),
+                        DropdownRow(
+                          title: "About Shoppe",
+                          onTap: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (_) => AboutUsScreen(),
+                              ),
+                            );
+                          },
+                        ),
+                      ],
+                    ),
+                  ),
+
+                  // SizedBox(height: 50.h),
+                  // Align(
+                  //   alignment: Alignment.centerLeft,
+                  //   child: GestureDetector(
+                  //     onTap: () {
+                  //       showDialog(
+                  //         context: context,
+                  //         builder: (context) {
+                  //           return Dialog(
+                  //             backgroundColor: Color(0xFFFFFFFF),
+                  //             shape: RoundedRectangleBorder(
+                  //               borderRadius: BorderRadius.circular(16),
+                  //             ),
+
+                  //             child: SizedBox(
+                  //               width: double.infinity,
+                  //               child: Padding(
+                  //                 padding: const EdgeInsets.all(20),
+                  //                 child: Column(
+                  //                   mainAxisSize: MainAxisSize.min,
+                  //                   children: [
+                  //                     Image.asset(
+                  //                       "assets/images/bluenav-btn.png",
+                  //                       width: 40.w,
+                  //                       height: 40.h,
+                  //                     ),
+                  //                     Text(
+                  //                       textAlign: TextAlign.center,
+                  //                       "You are going to delete your account",
+                  //                       style: TextStyle(
+                  //                         fontSize: 20.sp,
+                  //                         fontWeight: FontWeight.bold,
+                  //                       ),
+                  //                     ),
+                  //                     Text(
+                  //                       textAlign: TextAlign.center,
+                  //                       "You won't be able to restore your data",
+                  //                       style: TextStyle(
+                  //                         fontSize: 15.sp,
+                  //                         fontWeight: FontWeight.bold,
+                  //                       ),
+                  //                     ),
+                  //                     Row(
+                  //                       mainAxisAlignment:
+                  //                           MainAxisAlignment.spaceBetween,
+                  //                       children: [
+                  //                         SizedBox(
+                  //                           width: 140.w,
+                  //                           child: ButtonWidget(
+                  //                             text: "Cancel",
+                  //                             onPressed: () {},
+                  //                             textcolor: Colors.white,
+                  //                             color: Colors.black,
+                  //                             height: 40.h,
+                  //                             width: double.infinity,
+                  //                           ),
+                  //                         ),
+                  //                         SizedBox(
+                  //                           width: 140.w,
+                  //                           child: ButtonWidget(
+                  //                             text: "Delete",
+                  //                             onPressed: () {},
+                  //                             textcolor: Colors.white,
+                  //                             color: Colors.redAccent,
+                  //                             height: 40.h,
+                  //                             width: double.infinity,
+                  //                           ),
+                  //                         ),
+                  //                       ],
+                  //                     ),
+                  //                   ],
+                  //                 ),
+                  //               ),
+                  //             ),
+                  //           );
+                  //         },
+                  //       );
+                  //     },
+                  //     child: Text(
+                  //       "Delete My Account",
+                  //       style: TextStyle(
+                  //         color: Colors.redAccent,
+                  //         fontSize: 20.sp,
+                  //         fontWeight: FontWeight.bold,
+                  //       ),
+                  //     ),
+                  //   ),
+                  // ),
                 ],
               ),
             ),
