86738b258bee4f2c9f52b42adb4accc7ade9ba2c
Adedance <uthmanadedance@gmail.com>
2026-07-14 23:20:51 +0100

    Create service for api

---
+++ lib/screens/user_side/services/api_service.dart
@@ -0,0 +1,38 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+class ApiService {
+  static const String baseUrl = "https://shopee-api-three.vercel.app/";
+
+  Future<Map<String, dynamic>> createOrder({
+  required String userId,
+  required List<Map<String, dynamic>> items,
+  required double total,
+}) async {
+
+  // Sending the request
+  final response = await http.post(
+    // sends the request to
+    Uri.parse("$baseUrl/api/orders"),
+    headers: {
+      "Content-Type": "application/json",
+    },
+
+    // request body
+    body: jsonEncode({
+      "userId": userId,
+      "items": items,
+      "total": total,
+    }),
+  );
+
+// reading the response
+  final data = jsonDecode(response.body);
+
+  if (response.statusCode == 200) {
+    return data;
+  } else {
+    throw Exception(data["message"] ?? "Failed to create order");
+  }
+}
+}
