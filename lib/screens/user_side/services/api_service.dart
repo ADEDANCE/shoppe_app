6c8d234ef5958fca02c4c6b3121b6f7fa556ef69
Adedance <uthmanadedance@gmail.com>
2026-07-15 18:33:28 +0100

    Create make payment fuction

--- lib/screens/user_side/services/api_service.dart
+++ lib/screens/user_side/services/api_service.dart
@@ -2,37 +2,55 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 
 class ApiService {
-  static const String baseUrl = "https://shopee-api-three.vercel.app/";
+  static const String baseUrl = "https://shopee-api-three.vercel.app";
 
   Future<Map<String, dynamic>> createOrder({
-  required String userId,
-  required List<Map<String, dynamic>> items,
-  required double total,
-}) async {
+    required String userId,
+    required List<Map<String, dynamic>> items,
+    required double total,
+  }) async {
+    // Sending the request
+    final response = await http.post(
+      // sends the request to
+      Uri.parse("$baseUrl/api/orders"),
+      headers: {"Content-Type": "application/json"},
 
-  // Sending the request
-  final response = await http.post(
-    // sends the request to
-    Uri.parse("$baseUrl/api/orders"),
-    headers: {
-      "Content-Type": "application/json",
-    },
+      // request body
+      body: jsonEncode({"userId": userId, "items": items, "total": total}),
+    );
 
-    // request body
-    body: jsonEncode({
-      "userId": userId,
-      "items": items,
-      "total": total,
-    }),
-  );
+    // reading the response
+    final data = jsonDecode(response.body);
 
-// reading the response
-  final data = jsonDecode(response.body);
-
-  if (response.statusCode == 200) {
-    return data;
-  } else {
-    throw Exception(data["message"] ?? "Failed to create order");
+    if (response.statusCode == 200) {
+      return data;
+    } else {
+      throw Exception(data["message"] ?? "Failed to create order");
+    }
   }
-}
+
+  Future<Map<String, dynamic>> createPayment({
+    required String orderId,
+    required String email,
+    required String name,
+  }) async {
+    // Sending the request
+    final response = await http.post(
+      // sends the request to
+      Uri.parse("$baseUrl/api/payments/flutterwave"),
+      headers: {"Content-Type": "application/json"},
+
+      // request body
+      body: jsonEncode({"orderId": orderId, "email": email, "name": name}),
+    );
+
+    // reading the response
+    final data = jsonDecode(response.body);
+
+    if (response.statusCode == 200) {
+      return data;
+    } else {
+      throw Exception(data["message"] ?? "Failed to make payment");
+    }
+  }
 }
